150df6f958627e55717a5d7c1a2507a9c9c1cb1d
rintha <rintharithu@gmail.com>
2022-10-26 21:22:34 +0530

    restaurant summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import  {Fragment} from "react";
+import { Fragment } from "react";
+
 import Header from "./components/Layouts/Header";
+import Meals from './components/Meals/Meals'
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import MealsSummary from './MealsSummary';
+
+
+const Meals = props => {
+    return (
+        <Fragment>
+            <MealsSummary />
+        </Fragment>
+    );
+
+};
+
+export default Meals;
